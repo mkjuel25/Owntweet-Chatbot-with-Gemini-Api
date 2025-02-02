53d340d16a16f9d2637ffc405700fafc58d3cd96
Mollick Juel <92368809+mkjuel25@users.noreply.github.com>
2025-02-02 15:05:42 +0600

    Update config.php

--- config.php
+++ config.php
@@ -8,8 +8,12 @@ $pass = "password";
 $gemini_api_key = "Gemini Api key";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $options = [
+      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ];
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass, $options);
+
 } catch(PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }

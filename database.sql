489496fe05466deda9766b9312026a0441453eed
Mollick Juel <92368809+mkjuel25@users.noreply.github.com>
2025-02-02 20:41:55 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,13 @@
+-- Check if a database is selected
+SELECT DATABASE();
+
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Create the messages table
 CREATE TABLE IF NOT EXISTS messages (
@@ -14,4 +17,4 @@ CREATE TABLE IF NOT EXISTS messages (
     response TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
